79e217ef6d167a7cc1db930e83fc58bb66cb6032
nv-dev-user <nv-dev@outlook.fr>
2026-07-09 17:27:17 +0200

    better tailwind fix

--- app/components/TemplateMenu.vue
+++ app/components/TemplateMenu.vue
@@ -48,7 +48,7 @@
       variant="subtle"
       trailing-icon="i-lucide-chevron-down"
       size="xs"
-      class="-mb-[6px] font-semibold rounded-full truncate"
+      class="-mb-1.5 font-semibold rounded-full truncate"
       :class="[open && 'bg-primary/15']"
       :ui="{
         trailingIcon: [
